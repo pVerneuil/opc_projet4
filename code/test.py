8e59ad6129d11daf0c0e70883986d2f8a1e857a7
kurasoo6 <verneuil.paul@gmail.com>
2022-01-21 06:53:44 +0100

    test

--- code/test.py
+++ code/test.py
@@ -1,12 +1,10 @@
-from mimetypes import init
-from nis import match
-from unittest import result
 import random 
 
 class Player:
-    def __init__(self,id,score=0) :
+    def __init__(self,id,elo,score=0) :
         self.id = id
         self.score = score
+        self.elo = elo
     def set_score(self,result):
         self.score+=result
 
@@ -15,11 +13,17 @@ players_id_sorted = [1,3,4,5,6,7,10,12,30,25,100] #lets assume they are already 
 number_of_round = 6
 number_of_round_played = 0
 players_list = []
-matches = {} #rename round
+rounds = {} #rename round
 results = {}
 # first round
+eloo = 3000
 for id in players_id_sorted:
-    players_list.append(Player(id))
+    players_list.append(Player(id,eloo))
+    eloo-= 10
+
+for i in range(1, number_of_round):
+    rounds[f'round{i}'] = []
+    results[f'round{i}'] = []
 #eventually loop here with sorted player list
 if not len(players_list)%2 == 0:
     player_in_this_round = players_list[:-1]
@@ -28,22 +32,31 @@ else : player_in_this_round = players_list
 
 #creating matches
 i = 0
-temp=[] #matches 
+matches=[]  
 for i in range(0, int(len(player_in_this_round)/2)):
     #fix playerA et find player B that valid
-    temp.append([player_in_this_round[i],player_in_this_round[len(player_in_this_round)-1-i]])
-matches[f'round{number_of_round_played+1}'] = temp
+    matches.append([player_in_this_round[i],player_in_this_round[len(player_in_this_round)-1-i]])
+rounds[f'round{number_of_round_played+1}'] = matches
 #check if players already played each other here
 #outpout matches
-for item in matches[(f'round{number_of_round_played+1}')]:
+for item in rounds[(f'round{number_of_round_played+1}')]:
     print(f'joueur : {item[0].id} VS joueur : {item[1].id}')
 
 #results
 a = []
-for matche in matches[(f'round{number_of_round_played+1}')]:
+for matche in rounds[(f'round{number_of_round_played+1}')]:
     r = random.randrange(0, 3, 1)
     a.append([r/2,1-r/2])
 results[f'round{number_of_round_played+1}'] = a
-print (results)
-
+print(results)
 #set scores
+for (result,matche) in zip(results[f'round{number_of_round_played+1}'], rounds[f'round{number_of_round_played+1}']) :
+    matche[0].set_score(result[0])
+    matche[1].set_score(result[1])
+#for player in players_list:
+#    print(f'le joueur {player.id} a un score de {player.score}')
+#    print(player.elo)
+
+test = sorted(players_list, key=lambda x : (x.score, x.elo ),reverse=True)
+#for player in test:
+#    print(f'id : {player.id}| score : {player.score} | elo {player.elo}')
